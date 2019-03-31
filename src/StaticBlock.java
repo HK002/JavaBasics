a383f79c47ff658de27dc42e688449300c3f409e
HK002 <kartik02manas@outlook.com>
2019-03-31 11:29:13 +0530

    Changes Made.

--- src/StaticBlock.java
+++ src/StaticBlock.java
@@ -2,11 +2,11 @@
 public class StaticBlock {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Kartik.");
+		System.out.println("Hello Kartik. This is a basic java program.");
 	}
 
 	static {
-		System.out.println("Static Block. This is executed once, when the class is loaded, even before maon method is called.");
+		System.out.println("Static Block. This is executed once, when the class is loaded, even before main method is called.");
 	}
 
 }
